3286ac62f7d90d73700efe061db785c75c9ac0db
Erza0838 <muhammaderza884@gmail.com>
2023-06-06 17:58:34 +0700

    Belajar poplymorphism

---
+++ PilihanMerekPrinter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_Printer
+{
+    internal class MerekPrinterInput
+    {
+        public string[] MerekPrinter = { "Epson", "Canon", "LasetJet" };
+        public string inputAngkaPilihan;
+
+        public void PilihanPrinter()
+        {
+            Console.WriteLine("Pilih Printer");
+            inputAngkaPilihan = Console.ReadLine();
+
+            for(int angka = 0; angka < MerekPrinter.Length; angka++)
+            {
+                Console.WriteLine(angka + MerekPrinter[angka]);
+            }
+        }
+    }
+
+    internal class EpsonPrinter : MerekPrinterInput
+    {
+        public void TampilkanPesanEpsonPrinter()
+        {
+            Console.WriteLine("Epson display dimention : 10 * 13");
+        }
+    }
+
+    internal class CanonPrinter : MerekPrinterInput
+    {
+        public void TampilkanPesanCanonPrinter()
+        {
+            Console.WriteLine("Epson display dimention : 9.5 * 12");
+        }
+    }
+
+    internal class LasetJetPrinter : MerekPrinterInput
+    {
+        public void TampilkanPesanCanonPrinter()
+        {
+            Console.WriteLine("Epson display dimention : 9.5 * 12");
+        }
+    }
+}
