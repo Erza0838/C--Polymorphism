3950a6ea1e797f09c306d13e8a9816dbde81b452
Erza0838 <muhammaderza884@gmail.com>
2023-06-11 20:10:11 +0700

    Program selesai

---
+++ InputPrinter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] arg)
+    {
+       MerekPrinterInput InputMerekPrinter = new MerekPrinterInput();
+       OutputPilihanPrinter OutputPilihanPrinterObj = new OutputPilihanPrinter();
+
+       InputMerekPrinter.PilihanPrinter();
+       OutputPilihanPrinterObj.PilihanPrinter();
+    }
+  }
+}
--- PilihanMerekPrinter.cs
+++ PilihanMerekPrinter.cs
@@ -1,49 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Output_Printer
+﻿public class MerekPrinterInput
 {
-    internal class MerekPrinterInput
-    {
-        public string[] MerekPrinter = { "Epson", "Canon", "LasetJet" };
-        public string inputAngkaPilihan;
-
-        public void PilihanPrinter()
+    public string[] MerekPrinter = { "Epson", "Canon", "LasetJet" };
+    public string inputAngkaPilihan = "";
+    
+    public virtual void PilihanPrinter()
+    {   
+        for(int angka = 0; angka < 3; angka++)
         {
-            Console.WriteLine("Pilih Printer");
-            inputAngkaPilihan = Console.ReadLine();
-
-            for(int angka = 0; angka < MerekPrinter.Length; angka++)
-            {
-                Console.WriteLine(angka + MerekPrinter[angka]);
-            }
+            Console.WriteLine(angka + ". " + MerekPrinter[angka]);
         }
     }
+}
+public class OutputPilihanPrinter : MerekPrinterInput
+{   
+    public override void PilihanPrinter()
+    {   
+        Console.WriteLine("Pilih Printer [0..2] : ");
+        inputAngkaPilihan = Console.ReadLine();
 
-    internal class EpsonPrinter : MerekPrinterInput
-    {
-        public void TampilkanPesanEpsonPrinter()
-        {
-            Console.WriteLine("Epson display dimention : 10 * 13");
+        switch(inputAngkaPilihan)
+        {   
+            case "0" : Console.WriteLine("Epson display dimention : 10 * 13");
+            break;
+            case "1" : Console.WriteLine("Canon display dimention : 9.5 * 12");
+            break;
+            case "2" : Console.WriteLine("Canon display dimention : 9.5 * 13");
+            break;
         }
-    }
-
-    internal class CanonPrinter : MerekPrinterInput
-    {
-        public void TampilkanPesanCanonPrinter()
-        {
-            Console.WriteLine("Epson display dimention : 9.5 * 12");
-        }
-    }
-
-    internal class LasetJetPrinter : MerekPrinterInput
-    {
-        public void TampilkanPesanCanonPrinter()
-        {
-            Console.WriteLine("Epson display dimention : 9.5 * 12");
-        }
-    }
+    }   
 }
